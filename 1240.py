711231b083504fc5a3052f358ce772e85f36d57e
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-03-03 23:23:32 -0300

    atualizacao

--- 1240.py
+++ 1240.py
@@ -1,8 +1,5 @@
 n = int(input())
 for i in range(n):
     A, B = input().split()
-    A, B = int(A), int(B)
-    while (A >= B and A%10 == B%10) and B != 0:
-        A //= 10
-        B //= 10
-    print('encaixa' if B == 0 else 'nao encaixa')
+    A = int(A)
+    print('encaixa' if A % 10**len(B) else 'nao encaixa')
---
+++ 2818.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(n):
+    num = input()
+    count = int()
+    if num not in range(1, num):
+        if int(num) % 6 == 0 or int(num) == 0:
+            print(count)
+    elif 
