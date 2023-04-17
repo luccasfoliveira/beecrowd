6eebcaa7fd232be5af3381a0b79450c520899fba
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-04-17 15:50:45 -0300

    Update 3024.py
    404

--- 3024.py
+++ 3024.py
@@ -1,9 +1,10 @@
 n = input().split()
 metros = int(n[1])
 n = list(map(int, input().split()))
-x = count = 0
+x = 0
+count = 1
 for i in n[1:]:
-    if i - n[x] <= 2:
+    if n[x] <= i + 2:
         count += 1
     x += 1
 print(count)
