14777863af6093864294d82b9412afa00e2ef3da
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-04-19 13:41:17 -0300

    Update 3058.py

--- 3058.py
+++ 3058.py
@@ -1,10 +1,10 @@
 n = int(input())
 for i in range(n):
     a, b = input().split()
-    a, b = float(a), int(b)
+    c = float(a) * 1000 / int(b)
 
     if i == 0:
-        menor = a * 1000 / b
-    elif a * 1000 / b < menor:
-        menor = a * 1000 / b
+        menor = c
+    elif c < menor:
+        menor = c
 print(f'{menor:.2f}')
